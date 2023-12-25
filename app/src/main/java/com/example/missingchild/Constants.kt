d6c1bd0be369fed9549897a5a3852ee8f42f2452
Bhovan-Dasari <bhovan.dasari@solomonsint.com>
2023-12-25 12:26:11 +0530

    intial registration

---
+++ app/src/main/java/com/example/missingchild/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.missingchild
+
+object Constants  {
+//    val FLASK_BASE_URL = "http://192.168.1.100:5000"
+    val FLASK_BASE_URL = "https://5c6b-103-159-32-34.ngrok-free.app"
+}
